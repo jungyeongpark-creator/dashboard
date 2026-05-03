0da78b793a7efb8794317ba29b8013be5f3ada66
jungyeongpark-creator <jungyeong.park@seah.co.kr>
2026-05-03 21:34:17 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'budget-v1';
+const ASSETS = ['./budget.html', './icon-192.png', './icon-512.png', './manifest.json'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
